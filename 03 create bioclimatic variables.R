aa58ba857797f4f9e2e8bcbfa2fb38e918d4d23d
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-06-16 18:15:12 -0500

    update

--- 03 create bioclimatic variables.R
+++ 03 create bioclimatic variables.R
@@ -1,22 +1,20 @@
 
-
-
 # Load libraries ----------------------------------------------------------
 require(pacman)
-pacman::p_load(terra, sf, dismo, tidyverse, gtools, rgeos, raster, fs, glue)
+pacman::p_load(terra, sf, dismo, crayon, tidyverse, gtools, rgeos, raster, fs, glue)
 
 g <- gc(reset = T)
 rm(list = ls())
 
 # Function ----------------------------------------------------------------
-mdl <- mdls[1]
+mdl <- gcm_ms[1]
 prd <- prds[1]
 
 make_bios <- function(mdl, prd){
   
   # Filtering the folder for the model
   cat(mdl, prd, '\n', sep = ' ')
-  fld <- grep(mdl, mdls, value = T) %>% as.character() %>% glue('/{prd}')
+  fld <- grep(paste0(mdl, '$'), mdls, value = T) %>% as.character() %>% glue('/{prd}')
   fls <- dir_ls(fld) %>% as.character()
   
   # Read as raster stack
@@ -29,7 +27,7 @@ make_bios <- function(mdl, prd){
   
   # To write these rasters
   out <- glue('{fld}/bioc.tif')
-  raster::writeRaster(x = bio, filename = out, ovewrite = TRUE)
+  raster::writeRaster(x = bio, filename = out, overwrite = TRUE)
   cat(green('Finish!'), '\n')
   
 }
@@ -46,11 +44,26 @@ prds <- c('2021-2040', '2041-2060')
 # To make the bioclimatic variables ---------------------------------------
 
 # Period 2021-2040
-purrr::map(.x = 1:length(mdls), .f = function(m){
+purrr::map(.x = 1:length(gcm_ms), .f = function(m){
   
-  make_bios(mdl = mdls[m], prd = prds[1])
+  make_bios(mdl = gcm_ms[m], prd = prds[1])
   
 })
+
+# Check missing models ----------------------------------------------------
+gcm_ok <- purrr::map(.x = 1:length(mdls), .f = function(i){
+  
+  mdls[i] %>% 
+    dir_ls() %>% 
+    grep('2021-2040', ., value = T) %>% 
+    as.character() %>% 
+    dir_ls(., regexp = '.tif$') %>% 
+    grep('bio', ., value = T) %>% 
+    as.character()
+  
+}) %>% unlist() %>% str_split(., pattern = '/') %>% map(7) %>% unlist()
+
+gcm_ms <- setdiff(basename(mdls), gcm_ok)
 
 # Period 2041-2060
 purrr::map(.x = 1:length(mdls), .f = function(m){
