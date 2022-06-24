621aeb23f9d83fa3c120e2b742eee544a05d69b5
fabiolexcastro <fabioalexandercastro@gmail.com>
2022-06-24 16:32:02 -0500

    diference map

--- 07 difference map.R
+++ 07 difference map.R
@@ -1,13 +1,22 @@
 
 # Load libraries ----------------------------------------------------------
 require(pacman)
-pacman::p_load(terra, dismo, RColorBrewer, geosphere, rgeos, maptools, maps, mapdata, randomForest, sf, rgbif, ggspatial, geodata, readxl, ggrepel, dismo, rnaturalearthdata, rnaturalearth, readxl, crayon, tidyverse, gtools, rgeos, raster, fs, glue)
+pacman::p_load(terra, hrbrthemes, dismo, ggspatial, RColorBrewer, geosphere, rgeos, maptools, maps, mapdata, randomForest, sf, rgbif, ggspatial, geodata, readxl, ggrepel, dismo, rnaturalearthdata, rnaturalearth, readxl, crayon, tidyverse, gtools, rgeos, raster, fs, glue)
 
 g <- gc(reset = TRUE)
 rm(list = ls())
 options(scipen = 999, warn = -1)
 
 # Load data --------------------------------------------------------------
+
+# Administrative data
+
+chle <- geodata::gadm(country = 'CHL', level = 1, path = './tmpr') %>% st_as_sf()
+zone <- st_read('./gpkg/study_zone.gpkg')
+zone <- st_transform(zone, st_crs(4326))
+pnts <- read_csv('./tbl/points/points_vociferator.csv')
+
+# Results data
 bsln <- raster('./rf/run_1/mean_models.tif')
 ftre <- raster('./rf/run_1/ftr_2021_2040_mean_models.tif')
 
@@ -24,14 +33,32 @@ colnames(tble) <- c('x', 'y', 'baseline', 'f2021_2040')
 
 tble <- tble %>% gather(period, value, -x, -y)
 tble <- tble %>% mutate(period = factor(period, levels = c('baseline', 'f2021_2040')))
+tble <- mutate(tble, period = ifelse(period == 'baseline', 'Línea base', 'SSP 370 2021 - 2040'))
+tble <- mutate(tble, period = factor(period, levels = c('Línea base', 'SSP 370 2021 - 2040')))
 
-
-ggplot(data = tble, aes(x = x, y = y, fill = value)) + 
-  geom_tile() +
-  scale_fill_gradientn(colors = brewer.pal(n = 9, name = 'YlOrRd')) +
+gmap <- ggplot() + 
+  geom_tile(data = tble, aes(x = x, y = y, fill = value)) +
   facet_wrap(.~period) +
-  coord_sf() + 
+  scale_fill_gradientn(colors = brewer.pal(n = 9, name = 'RdYlGn')) +
+  geom_sf(data = zone, fill = NA, col = 'grey50', lwd = 0.5) + 
+  geom_point(data = pnts, aes(x = LONGITUD, y = LATITUD), size = 0.5, col = 'black') +
+  coord_sf(ylim = c(-40, -35)) + 
+  theme_ipsum_es() +
+  labs(x = 'Lon', y = 'Lat', fill = 'Idoneidad') +
   theme(legend.position = 'bottom', 
-        legend.key.width = unit(2, 'line'))
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        legend.key.width = unit(5, 'line'), 
+        strip.text = element_text(face = 'bold', hjust = 0.5), 
+        axis.text.x = element_text(size = 6), 
+        axis.text.y = element_text(size = 6)) +
+  annotation_scale(location =  "br", width_hint = 0.4) +
+  annotation_north_arrow(location = "tl", which_north = "true", 
+                         pad_x = unit(0.05, "in"), pad_y = unit(0.1, "in"), 
+                         style = north_arrow_fancy_orienteering())
+
+ggsave(plot = gmap, filename = './png/maps/baseline_ssp370_2021_2040.png', 
+       units = 'in', width = 9, height = 12, dpi = 300)
+
 
 
